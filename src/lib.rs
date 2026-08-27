618298ec44f1577b4d9d310ecc442d0fe9ee10fc
Arnaud Jezequel <arnaud.jezequel@gmail.com>
2026-08-27 15:35:36 +0000

    Bump plugin version to 0.7.0

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ pub fn infos() -> FnResult<Json<PluginInformation>> {
     Ok(Json(PluginInformation {
         name: "openlibrary_metadata".into(),
         capabilities: vec![PluginType::LookupMetadata],
-        version: 6,
+        version: 7,
         interface_version: 1,
         repo: Some("https://github.com/neckaros/rs-plugin-openlibrary".into()),
         publisher: "neckaros".into(),
